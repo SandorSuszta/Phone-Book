e88617156efe57a94c4561f306a7405d8dde9257
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-17 21:22:58 +0100

    Add constraints for ContactCell

--- Phone Book/Views/ContactCell.swift
+++ Phone Book/Views/ContactCell.swift
@@ -27,3 +27,44 @@ final class ContactCell: UITableViewCell {
         return label
     }()
 }
+
+    //MARK: - Setup Views
+
+extension ContactCell {
+    
+    private enum Constants {
+        static let padding: CGFloat = 16
+        static let smallPadding: CGFloat = 8
+    }
+    
+    private func setupViewHierarchy() {
+        contentView.addSubview(avatarImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(phoneLabel)
+    }
+    
+    private func setupConstraints() {
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        phoneLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            // Avatar image view constraints
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
+            avatarImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            avatarImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, constant: -Constants.smallPadding),
+            avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor),
+            
+            // Name label constraints
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: Constants.padding),
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.padding),
+            
+            // Phone label constraints
+            phoneLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            phoneLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: Constants.smallPadding),
+            phoneLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.padding)
+        ])
+    }
+}
+
