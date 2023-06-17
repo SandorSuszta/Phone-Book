b2f17f4f2917eea8eb7e862c1bf62bd1948a1ce8
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-17 20:43:43 +0100

    Add nav bar buttons

--- Phone Book/ViewControllers/ContactsViewController.swift
+++ Phone Book/ViewControllers/ContactsViewController.swift
@@ -1,9 +1,65 @@
 import UIKit
 
-class ContactsViewController: UIViewController {
+final class ContactsViewController: UIViewController {
+    
+    //MARK: - UI Elements
+    
+    private lazy var contactsTableView: UITableView = {
+        let table = UITableView()
+        return table
+    }()
 
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupVC()
+        setupNavBar()
     }
 }
+
+    //MARK: - Private methods
+
+extension ContactsViewController {
+    
+    private func setupVC() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupNavBar() {
+        title = "AdressBook"
+        addContactButton()
+        addSettingsButton()
+    }
+    
+    private func addContactButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(didTapAddContact)
+        )
+    }
+    
+    private func addSettingsButton() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            style: .plain,
+            target: self,
+            action: #selector(didTapAddCSettings))
+    }
+}
+
+    //MARK: - Selectors
+
+extension ContactsViewController {
+    
+    @objc private func didTapAddContact() {
+        //TODO: handle tap
+    }
+    
+    @objc private func didTapAddCSettings() {
+        //TODO: handle tap
+    }
+}
+
+
