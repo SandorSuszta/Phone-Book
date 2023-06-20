4d308571427ac846cc168381944534874841cfcd
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-20 13:03:54 +0100

    Add gesture recogniser to avatar

--- Phone Book/ViewControllers/NewContactViewController.swift
+++ Phone Book/ViewControllers/NewContactViewController.swift
@@ -10,10 +10,8 @@ final class NewContactViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
         imageView.isUserInteractionEnabled = true
-        //let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseAvatar))
-        imageView.addGestureRecognizer(tapGesture)
         return imageView
-    }
+    }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
@@ -58,12 +56,17 @@ extension NewContactViewController {
     private func setupNavBar() {
         title = "Add new contact"
     }
-}
-
-//MARK: - Selectors
-
-extension ContactsViewController {
     
+    private func addGestureRecogniserToAvatarView() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseAvatar))
+        avatarImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    //MARK: - Selectors
+    
+    @objc private func chooseAvatar() {
+        
+    }
 }
 
 //MARK: - Setub Views
@@ -72,13 +75,46 @@ extension NewContactViewController {
     
     private func setupViewHierarchy() {
         view.addSubview(avatarImageView)
-            view.addSubview(nameLabel)
-            view.addSubview(nameTextField)
-            view.addSubview(phoneLabel)
-            view.addSubview(phoneTextField)
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(phoneLabel)
+        view.addSubview(phoneTextField)
     }
     
     private func setupConstraints() {
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        phoneLabel.translatesAutoresizingMaskIntoConstraints = false
+        phoneTextField.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            // Avatar image view constraints
+            avatarImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 100),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            // Name label constraints
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            // Name text field constraints
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            // Phone label constraints
+            phoneLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 20),
+            phoneLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            phoneLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            // Phone text field constraints
+            phoneTextField.topAnchor.constraint(equalTo: phoneLabel.bottomAnchor, constant: 8),
+            phoneTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            phoneTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
+    
 }
