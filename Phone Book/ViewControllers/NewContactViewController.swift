5a8aa699c9ec4da9231636c7305a8ea4b0cdb09c
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-20 12:59:17 +0100

    Add UI elements to NewContact VC

--- Phone Book/ViewControllers/NewContactViewController.swift
+++ Phone Book/ViewControllers/NewContactViewController.swift
@@ -4,16 +4,52 @@ final class NewContactViewController: UIViewController {
     
     //MARK: - UI Elements
     
+    private var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        //let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseAvatar))
+        imageView.addGestureRecognizer(tapGesture)
+        return imageView
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        return label
+    }()
+    
+    let phoneLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Phone"
+        return label
+    }()
+    
+    private var nameTextField = {
+        let textField = UITextField()
+        textField.placeholder = "e.g. Sam"
+        return textField
+    }()
+    
+    private var phoneTextField = {
+        let textField = UITextField()
+        textField.placeholder = "e.g 447491927343"
+        return textField
+    }()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewHierarchy()
     }
 }
 
-    //MARK: - Private methods
+//MARK: - Private methods
 
-extension ContactsViewController {
+extension NewContactViewController {
     
     private func setupVC() {
         view.backgroundColor = .systemBackground
@@ -24,8 +60,25 @@ extension ContactsViewController {
     }
 }
 
-    //MARK: - Selectors
+//MARK: - Selectors
 
 extension ContactsViewController {
     
+}
+
+//MARK: - Setub Views
+
+extension NewContactViewController {
+    
+    private func setupViewHierarchy() {
+        view.addSubview(avatarImageView)
+            view.addSubview(nameLabel)
+            view.addSubview(nameTextField)
+            view.addSubview(phoneLabel)
+            view.addSubview(phoneTextField)
+    }
+    
+    private func setupConstraints() {
+        
+    }
 }
